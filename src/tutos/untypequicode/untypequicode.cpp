a3db7789c686a6b02320d5d13283dde2767fcab3
Ethan Facca <untypequicode@gmail.com>
2023-09-15 11:01:18 +0200

    ajout de grestion pls fichiers

---
+++ src/tutos/untypequicode/untypequicode.cpp
@@ -0,0 +1,47 @@
+#include <untypequicode.h>
+
+
+
+void untypequicode() {
+	float data1 = 0.0f;
+	float data2 = 0.0f;
+	std::cout << "0 : finir le programme" << std::endl;
+	std::cout << "1 : addition" << std::endl;
+	std::cout << "2 : soustraction" << std::endl;
+	std::cout << "3 : multiplication" << std::endl;
+	std::cout << "4 : division" << std::endl;
+	int input = 0;
+	std::cin >> input;
+	std::cin.ignore();
+	if (input == 0)
+	{
+		std::cout << "Fin du programme" << std::endl;
+		return 0;
+	}
+	std::cout << "Entrez le premier nombre : ";
+	std::cin >> data1;
+	std::cin.ignore();
+	std::cout << "Entrez le deuxième nombre : ";
+	std::cin >> data2;
+	std::cin.ignore();
+	if (input == 1)
+	{
+		std::cout << data1 << " + " << data2 << " = " << data1 + data2 << std::endl;
+	}
+	else if (input == 2)
+	{
+		std::cout << data1 << " - " << data2 << " = " << data1 - data2 << std::endl;
+	}
+	else if (input == 3)
+	{
+		std::cout << data1 << " X " << data2 << " = " << data1 * data2 << std::endl;
+	}
+	else if (input == 4)
+	{
+		std::cout << data1 << " / " << data2 << " = " << data1 / data2 << std::endl;
+	}
+	else
+	{
+		std::cout << "Erreur" << std::endl;
+	}
+};
---
+++ src/tutos/untypequicode/untypequicode.h
@@ -0,0 +1,8 @@
+#ifndef DEF_UNTYPEQUICODE
+#define DEF_UNTYPEQUICODE
+
+#include <iostream>
+
+void untypequicode();
+
+#endif
