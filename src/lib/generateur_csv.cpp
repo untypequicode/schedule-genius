25cc435af88a60a46b50a3cd70d563928cb1bc09
PGII33 <mickycomptepro@gmail.com>
2023-09-22 11:03:15 +0200

    version 3.141592 de csv

--- src/lib/generateur_csv.cpp
+++ src/lib/generateur_csv.cpp
@@ -2,9 +2,28 @@
 
 void creerCsv()
 {
+
+	int NOMBRE_ELEVE = 200;
+	int NOMBRE_PRENOM = PRENOM->size();
+	int NOMBRE_NOM = NOM->size();
+	int NOMBRE_MATIERE = MATIERE_SPE->size();
+
 	std::ofstream myfile;
 	myfile.open("example.txt");
-	myfile << "Writing this to a file.\n";
+	for (int i = 0; i < 10; i++)
+	{
+		myfile << MATIERE_SPE[i];
+	};
+	for (int i = 0; i < NOMBRE_ELEVE; i++)
+	{
+		myfile << PRENOM[rand() % NOMBRE_PRENOM] << ";";
+		myfile << NOM[rand() % NOMBRE_NOM] << ";";
+		for (int e = 0; e < NOMBRE_MATIERE; e++)
+
+		{
+
+		};
+	}
 	myfile.close();
 	std::cout << "Fini" << std::endl;
 }
