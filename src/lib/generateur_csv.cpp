ff5795230259505666807eae5f7f1beee63e85e2
PGII33 <mickycomptepro@gmail.com>
2023-09-24 09:14:25 +0200

    J'ai try hard, normalement le csv c'est fait

--- src/lib/generateur_csv.cpp
+++ src/lib/generateur_csv.cpp
@@ -1,35 +1,44 @@
 #include "generateur_csv.hpp"
 
+
+std::string none_ = "None";
+
 void creerCsv()
 {
-
 	int NOMBRE_ELEVE = 200;
-	int NOMBRE_PRENOM = PRENOM->size();
-	int NOMBRE_NOM = NOM->size();
-	int NOMBRE_MATIERE = MATIERE_SPE->size();
+	int NOMBRE_PRENOM = PRENOM.size();
+	int NOMBRE_NOM = NOM.size();
+	int NOMBRE_MATIERE = MATIERE_SPE.size();
 
 	std::ofstream myfile;
-	myfile.open("example.txt");
-	for (int i = 0; i < 10; i++)
+	myfile.open("example.csv");
+
+	// Première ligne du csv avec les différentes catégories
+	myfile << " PRENOM;" << "NOM;" << "Annee;";
+	for (int i = 1; i < 11; i++)
 	{
-		myfile << MATIERE_SPE[i] << " ; ";
+		myfile << "MATIERE OPTIONNELLE" << i << ";";
 	};
+	myfile << "\n";
+
+	// Autres lignes du csv
 	for (int i = 0; i < NOMBRE_ELEVE; i++)
 	{
-		myfile << PRENOM[rand() % NOMBRE_PRENOM] << " ; ";
-		myfile << NOM[rand() % NOMBRE_NOM] << " ; " << "\n";
+		myfile << PRENOM[rand() % NOMBRE_PRENOM] << ";";
+		myfile << NOM[rand() % NOMBRE_NOM] << ";";
+		myfile << rand() % 3 + 1 << ";";
 
-		std::string matiere_restante[15] = {};
-		for (std::string element : matiere_restante)
-		{
-			int index = 0;
-			matiere_restante[index] = MATIERE_SPE[index];
-		}
-		for (int e = 0; e < NOMBRE_MATIERE; e++)
+		int NOMBRE_MATIERE_SPE = rand() % 10;
+
+		std::vector <std::string> matiere_restante = MATIERE_SPE;
+		for (int e = 0; e < NOMBRE_MATIERE_SPE; e++)
 
 		{
-			
+			int RANDOM_MATIERE = rand() % 5;
+			myfile << matiere_restante[RANDOM_MATIERE] << ";";
+			matiere_restante.erase(matiere_restante.begin() + RANDOM_MATIERE);
 		};
+		myfile << "\n";
 	}
 	myfile.close();
 	std::cout << "Fini" << std::endl;
--- src/lib/generateur_csv.hpp
+++ src/lib/generateur_csv.hpp
@@ -3,17 +3,22 @@
 
 #include <iostream>
 #include <fstream>
+#include <list>
+#include <vector>
 
 void creerCsv();
-std::string const PRENOM[] = {"Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Hannah", "Ivy", "Jack",
+
+std::string None = "None";
+
+std::vector <std::string> PRENOM = {"Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Hannah", "Ivy", "Jack",
 "Katherine", "Liam", "Mia", "Noah", "Olivia", "Peter", "Quinn", "Rachel", "Samuel", "Taylor",
 "Ursula", "Victor", "Wendy", "Xavier", "Yasmine", "Zane" };
 
-std::string const NOM[] = {"Dupont", "Martin", "Dubois", "Bernard", "Thomas", "Robert", "Richard", "Petit", "Durand", "Leroy",
+std::vector <std::string> NOM = {"Dupont", "Martin", "Dubois", "Bernard", "Thomas", "Robert", "Richard", "Petit", "Durand", "Leroy",
 "Moreau", "Simon", "Laurent", "Lefebvre", "Michel", "Garcia", "David", "Roux", "Bonnet", "Fournier",
 "Lopez", "Legrand", "Martinez", "Ferreira", "Fernandez", "Blanc", "André", "Leroux", "Rousseau" };
 
-std::string const MATIERE_SPE[] = {"Francais", "Histoire" "Geo", "EMC", "EPS", "SPC", "SVT", "Maths", "NSI", "Philo",
+std::vector <std::string> MATIERE_SPE = {"Francais", "Histoire" "Geo", "EMC", "EPS", "SPC", "SVT", "Maths", "NSI", "Philo",
 "Anglais", "Espagnole", "Allemand", "Italien", "Russe" };
 
 #endif
