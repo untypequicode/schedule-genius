69aa4c382978ec8df8650c5c0a2a1dd4060dafa6
Vincentcestmoi <99179037+Vincentcestmoi@users.noreply.github.com>
2023-09-25 13:49:45 +0200

    création lecture par caractère

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include "lib/eleves.hpp"
 #include "lib/groupes.hpp"
 #include "lib/generation_groupes.hpp"
-#include "lib/generateur_csv.hpp"
+//#include "lib/generateur_csv.hpp"
 #include "lib/import.hpp"
 
 #include <iostream>
-
+;
 int main()
 {
 
-	/*creerCsv();
+	//creerCsv();
 	std::string nom_fichier = "example.txt";
-	ouvrirFichierParLigne(nom_fichier);
+	//ouvrirFichierParLigne(nom_fichier);
 	//ouvrirFichierParMot(nom_fichier);
+	ouvrirFichierParCaractere(nom_fichier);
 	/*Eleve eleve("Ethan", "Facca", "L1", 10);
 	eleve.SetMatiere("Maths", 1);
 	std::cout << eleve.GetMatiere(1) << std::endl;*/
