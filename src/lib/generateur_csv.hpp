f93a4b8012f28645b197add1dfb425b73a9f3098
PGII33 <mickycomptepro@gmail.com>
2023-09-29 12:05:17 +0200

    bug mais update quand même

--- src/lib/generateur_csv.hpp
+++ src/lib/generateur_csv.hpp
@@ -3,9 +3,30 @@
 
 #include <fstream>
 #include <iostream>
+#include <cstdlib>
 #include "tab_dyn.hpp"
 
-void begin();
-void creerCsv();
+class CsvGenerateur
+{
+	public:
+		CsvGenerateur();
+		~CsvGenerateur();
+
+		void creerCsv();
+
+		unsigned int M_NOMBRE_ELEVE;
+
+	private:
+		TabDynString m_prenom;
+		TabDynString m_nom;
+		TabDynString m_matiere_spe;
+		TabDynString m_matiere_restante;
+		unsigned int m_id_eleve;
+
+		unsigned int NOMBRE_MATIERE_SPE;
+		unsigned int M_NOMBRE_NOM;
+		unsigned int M_NOMBRE_PRENOM;
+		unsigned int M_NOMBRE_MATIERE_SPE;
+};
 
 #endif
