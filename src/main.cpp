0c721ce3c5ed92d76081e62039e787233910508d
Vincentcestmoi <99179037+Vincentcestmoi@users.noreply.github.com>
2023-10-02 14:25:47 +0200

    detail (suppression ligne main)

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,9 @@ int main()
 
 	//// Vincent
 	//
-	std::string nom_fichier = "eleve.csv";
+	//std::string nom_fichier = "eleve.csv";
 	//genererEleveViaCsv(nom_fichier);
-	Fichier fichiertest(nom_fichier);
+	//Fichier fichiertest(nom_fichier);
 	//fichiertest.GetTexte();
 	//std::cout << convertString("525") + convertString("58");
 
