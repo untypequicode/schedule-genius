2cf0db7b0d9489c10ad8a5a52068010f6e1c179d
Ethan Facca <untypequicode@gmail.com>
2023-10-05 13:40:19 +0200

    update TabDynEleve dans Eleve

--- src/lib/eleves.hpp
+++ src/lib/eleves.hpp
@@ -35,4 +35,30 @@ class Eleve
 
 };
 
+class TabDynEleve : public TabDyn
+{
+public:
+	TabDynEleve();
+	TabDynEleve(unsigned int nbElemMax);
+	TabDynEleve(unsigned int nbElemMax, bool add_with_multiple, unsigned int addition);
+	TabDynEleve(const TabDynEleve& tabDynEleve);
+	~TabDynEleve();
+
+	void Append(Eleve i);
+	void Add(Eleve i);
+	Eleve Get(unsigned int index) const;
+	Eleve* GetTab() const;
+	void Set(unsigned int index, Eleve i);
+	Eleve Pop();
+	Eleve Pop(int index);
+	void Remove(Eleve elem, int num);
+	void Remove(Eleve elem);
+	void Remove(Eleve elem, bool first);
+
+private:
+	void AddAppend(Eleve i, bool addition);
+
+	Eleve* m_tab;
+};
+
 #endif
