a094a77b60557972b54e6da943b39a8d309ea7e8
Vincentcestmoi <99179037+Vincentcestmoi@users.noreply.github.com>
2023-10-17 18:22:21 +0200

    Recréation du système de création de donnés à partir d'un fichier, à moitié codé, marche mais qu'une fois

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,11 @@ int main()
     Fichier fichiertest(nom_fichier, 201);
     Database datatest;
     datatest.EcraserData(fichiertest);
+    std::cout << "fin" << std::endl;
+    for(int i = 0; i < datatest.GetEleve(0).GetNbMatiere(); i++)
+    {
+        std::cout << datatest.GetEleve(0).GetMatiere(i) << std::endl;
+    }
 
 
     //// Ethan
