a2eb79d81d810fc049aebb859f2668683e2ce0bd
PGII33 <mickycomptepro@gmail.com>
2023-10-27 10:15:13 +0200

    CD:  - Correction bug du split (privé)  - Oublie de self.  - assert type

--- src/lib/change_occ/cd.py
+++ src/lib/change_occ/cd.py
@@ -7,21 +7,26 @@ class Cd:
 
     def Ls(self, path) -> list:
         """ return the list of directories """
-        return os.listdir(m_path + path)
+        assert isinstance(path, str), " Le chemin doit être un string "
+        return os.listdir(self.m_path + path)
     def __Split_path(self, path):
+        """ Split the path """
         up_list = []
         while path[3:] == "../":
-            up_list += path[3:]
-            path = path[3:]
+            up_list += path[2:]
+            path = path[2:]
         return up_list, path
 
     def Exist(self, path) -> bool:
+        assert isinstance(path, str), " Le chemin doit être un string "
         """ Return a bool"""
         up_list, path = self.__Split_path(path)
         return os.path.exists(up_list + path)
 
-    def Cd(self, path) -> string :
-        """ return  """
+    def Cd(self, path) -> str :
+        """ return False if  """
+        assert isinstance(path, str), " Le chemin doit être un string "
+
         up_list, path = self.__Split_path(path)
         if not os.path.exists(up_list + self.m_path + path):
             return False
