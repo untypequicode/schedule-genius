94aecb2052f99f20c7feec3f0cd9d9e2fc39fa33
untypequicode <untypequicod@gmail.com>
2023-10-27 08:49:24 +0200

    init change_occ.py - create FileChangeOcc

--- src/lib/change_occ/change_occ.py
+++ src/lib/change_occ/change_occ.py
@@ -1,11 +1,27 @@
 
 
 class FileChangeOcc:
-    def __init__(self, file_path, line, change_type, change):
-        self.file_path = file_path
-        self.line = line
-        self.change_type = change_type
-        self.change = change
+    def __init__(self, file_path):
+        self.m_file_path = file_path
+
+    def SetGilePath(self, file_path):
+        self.m_file_path = file_path
+
+    def GetGroup(self):
+        with open(self.m_file_path, "r") as f:
+            lignes = f.readlines()
+            group = []
+            for line in lignes:
+                if line[0] != "#":
+                    line = line.replace("\n", "")
+                    group.append([])
+                    split = line.split(",")
+                    for i in split:
+                        if i != "":
+                            group[-1].append(i)
+                    if group[-1] == []:
+                        group.pop(-1)
+            self.group = group
 
     def __str__(self):
-        return f"{self.file_path}:{self.line}: {self.change_type} {self.change}"
+        return f"{self.m_file_path}"
