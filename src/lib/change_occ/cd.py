e0ba8c3652d466848a9448301b74e8c84082243a
PGII33 <mickycomptepro@gmail.com>
2023-10-27 15:03:10 +0200

    CD:  - J'ai tout corrigé c'est ton tour Ethan

--- src/lib/change_occ/cd.py
+++ src/lib/change_occ/cd.py
@@ -9,7 +9,6 @@ class Cd:
         self.m_path = path
     def GetPath(self)-> str:
         return self.m_path
-
     def GetActualPath(self)-> str:
         return os.getcwd() + '/'
     def Ls(self) -> list:
@@ -25,27 +24,21 @@ class Cd:
             if os.path.isfile(os.path.join(self.m_path, elem)):
                 ls_sort["file"].append(elem)
         return ls_sort
-
-
     def __Split_path(self, path):
         """ Split the path """
-        copy_self_path = self.m_path
-        list_element_in_path = self.__Borned_by(copy_self_path, "/")
+        list_element_in_path = self.__Borned_by(self.m_path, "/")
         new_path = ""
         indice_max = len(list_element_in_path)
-        print(indice_max, list_element_in_path, copy_self_path)
         while path[:3] == "../":
             path = path[3:]
             indice_max -= 1
         for i in range(indice_max):
-            new_path += list_element_in_path[i]
+            new_path += list_element_in_path[i] + '/'
             self.m_path += list_element_in_path[i]
         new_path += path
-        print(new_path, "hey", copy_self_path)
+        print(new_path + 'hey')
         return new_path
-
     def __Borned_by(self, string, separator)-> str:
-
         new_list = []
         string_temp = ""
         for element in string:
@@ -59,16 +52,16 @@ class Cd:
     def Exist(self, path) -> bool:
         assert isinstance(path, str), " Le chemin doit être un string "
         """ Return a bool"""
-        up_list, path = self.__Split_path(path)
-        return os.path.exists(up_list + path)
+        return os.path.isdir(path) or os.path.isfile(path)
 
     def Cd(self, path) -> str :
-        """ return False if  """
+        """ return False if """
         assert isinstance(path, str), " Le chemin doit être un string "
-        path = self.__Split_path(path)
-        if not os.path.exists(path):
+        a = self.__Split_path(path)
+        if not self.Exist(a):
             return False
-        print(self.m_path, path)
+        path = a
+        self.m_path = path
         return path
 
 # test = Cd()
