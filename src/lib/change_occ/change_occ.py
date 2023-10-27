a8afeea3d81663cd4ceda9f10ed3cd8f8c337e30
untypequicode <untypequicod@gmail.com>
2023-10-27 09:56:50 +0200

    init Terminal

--- src/lib/change_occ/change_occ.py
+++ src/lib/change_occ/change_occ.py
@@ -4,24 +4,29 @@ class FileChangeOcc:
     def __init__(self, file_path):
         self.m_file_path = file_path
 
-    def SetGilePath(self, file_path):
+    def SetFilePath(self, file_path):
         self.m_file_path = file_path
 
     def GetGroup(self):
         with open(self.m_file_path, "r") as f:
-            lignes = f.readlines()
-            group = []
-            for line in lignes:
-                if line[0] != "#":
-                    line = line.replace("\n", "")
-                    group.append([])
-                    split = line.split(",")
-                    for i in split:
-                        if i != "":
-                            group[-1].append(i)
-                    if group[-1] == []:
-                        group.pop(-1)
-            self.group = group
+            file = []
+            for groupe in f.read().split("\n\n"):
+                groupe = groupe.split("\n")
+                for i in range(len(groupe) - 1, -1, -1):
+                    groupe[i] = groupe[i].split(",")
+                    for j in range(len(groupe[i]) - 1, -1, -1):
+                        groupe[i][j] = groupe[i][j].replace("\n", "")
+                        if groupe[i][j] == "":
+                            groupe[i].pop(j)
+                        if groupe[i][j][0] == "#":
+                            groupe[i].pop(j)
+                    if len(groupe[i]) == 0:
+                        groupe.pop(i)
+                if len(groupe) > 0:
+                    file.append(groupe)
+        self.m_group = file
+
+
 
     def __str__(self):
         return f"{self.m_file_path}"
