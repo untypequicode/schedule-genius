e0138f482561bc3b0ffd490c5308b0183acf3f0d
PGII33 <mickycomptepro@gmail.com>
2023-10-27 14:13:28 +0200

    CD:  - Borned by corrigé

--- src/lib/change_occ/cd.py
+++ src/lib/change_occ/cd.py
@@ -2,9 +2,9 @@ import os
 class Cd:
     def __init__(self, path = ""):
         if path == "":
-            self.m_path = os.getcwd() + "/"
+            self.m_path = os.getcwd().replace("\\","/") + "/"
         else:
-            self.m_path = path
+            self.m_path = path.replace("\\", "/")
     def SetPath(self, path):
         self.m_path = path
     def GetPath(self)-> str:
@@ -32,25 +32,28 @@ class Cd:
         copy_self_path = self.m_path
         list_element_in_path = self.__Borned_by(copy_self_path, "/")
         new_path = ""
-        indice_max = len(list_element_in_path) - 1
-        while path[2:] == "../":
-            path = path[2:]
+        indice_max = len(list_element_in_path)
+        print(indice_max, list_element_in_path, copy_self_path)
+        while path[:3] == "../":
+            path = path[3:]
             indice_max -= 1
         for i in range(indice_max):
             new_path += list_element_in_path[i]
             self.m_path += list_element_in_path[i]
         new_path += path
+        print(new_path, "hey", copy_self_path)
         return new_path
 
     def __Borned_by(self, string, separator)-> str:
+
         new_list = []
         string_temp = ""
         for element in string:
             if separator == str(element):
                 new_list.append(string_temp)
                 string_temp = ""
-            if string_temp != "":
-                string_temp.append(element)
+            else:
+                string_temp += element
         return new_list
 
     def Exist(self, path) -> bool:
@@ -65,6 +68,7 @@ class Cd:
         path = self.__Split_path(path)
         if not os.path.exists(path):
             return False
+        print(self.m_path, path)
         return path
 
 # test = Cd()
