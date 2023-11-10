d36283011213b674f77a1bbb2d79193e8c937ae2
untypequicode <untypequicod@gmail.com>
2023-11-10 13:01:37 +0100

    update load

--- src/lib/change_occ/load.py
+++ src/lib/change_occ/load.py
@@ -1,33 +1,83 @@
 class Load:
-    def __init__(self, path):
-        self.m_path = path
+    def __init__(self):
+        self.file_load = {"default": []}
+        
+    def GetList(self):
+        return self.file_load
 
-    def SetPath(self, path):
-        self.m_path = path
+    def SetInput(self, input, cd):
+        key = "default"
+        if len(input) > 0:
+            if input[0] == "key" and len(input) >= 2:
+                if input[1] not in self.file_load:
+                    self.file_load[input[1]] = []
+                key = input[1]
+                input = input[2:]
+        if len(input) > 0:
+            if (input[0] == "mount" or input[0] == "mnt") and len(input) >= 2:
+                if input[1] == "-a":
+                    self.__MountAll(input[2:], cd, key)
+                else:
+                    self.__Mount(input[1:], cd, key)
+                return self.file_load
+            elif (input[0] == "unmount" or input[0] == "umnt") and len(input) >= 2:
+                if input[1] == "-a":
+                    self.__UnmountAll(input[2:], cd, key)
+                else:
+                    self.__Unmount(input[1:], cd, key)
+                return self.file_load
+            elif input[0] == "list" or input[0] == "ls":
+                self.__List(key)
+                return self.file_load
+            elif input[0] == "key":
+                self.__ListKey()
+                return None
+            print(f'Commande "{input[0:]}" inconnue.')
+            return None
+        print("Error: input empty")
+        return None
 
-    def SetInput(self, path, input):
-        self.m_path = path
-        self.m_input = input
+    def __Mount(self, input, cd, key = "default"):
+        ls = cd.LsFile()
+        for file in input:
+            if file in ls and cd.GetPath() + file not in self.file_load[key]:
+                self.file_load[key].append(cd.GetPath() + file)
 
+    def __MountAll(self, input, cd, key = "default"):
+        ls = cd.LsFile()
+        if len(input) >= 2:
+            if input[0] == "\\":
+                for exclude in input[1:]:
+                    if exclude in ls:
+                        ls.remove(exclude)
+        for file in ls:
+            if cd.GetPath() + file not in self.file_load[key]:
+                self.file_load[key].append(cd.GetPath() + file)
 
-    def __Mount(self):
-        pass
+    def __Unmount(self, input, cd, key = "default"):
+        ls = cd.LsFile()
+        for file in input:
+            if file in ls and cd.GetPath() + file in self.file_load[key]:
+                self.file_load[key].remove(cd.GetPath() + file)
 
-    def __MountAll(self):
-        pass
+    def __UnmountAll(self, input, cd, key = "default"):
+        ls = cd.LsFile()
+        if len(input) >= 2:
+            if input[0] == "\\":
+                for exclude in input[1:]:
+                    if exclude in ls:
+                        ls.remove(exclude)
+        for file in ls:
+            if cd.GetPath() + file in self.file_load[key]:
+                self.file_load[key].remove(cd.GetPath() + file)
 
-    def __MountExcept(self):
-        pass
+    def __List(self, key = "default"):
+        if len(self.file_load[key]) == 0:
+            print("Aucun fichier n'est chargé.")
+        else:
+            for file in self.file_load[key]:
+                print(file)
 
-    def __Unmount(self):
-        pass
-
-    def __UnmountAll(self):
-        pass
-
-    def __UnmountExcept(self):
-        pass
-
-    def __List(self):
-        pass
-
+    def __ListKey(self):
+        for key in self.file_load:
+            print(key)
--- src/lib/change_occ/main.py
+++ src/lib/change_occ/main.py
@@ -1,6 +1,7 @@
 #from change_occ import FileChangeOcc
 from terminal import Terminal
 from cd import Cd
+from load import Load
 
 def main():
     # file = FileChangeOcc("change_occ.csv")
@@ -11,7 +12,7 @@ def main():
     terminal = Terminal()
     cd = Cd()
     response = terminal.GetInput(cd.GetPath())
-    file_load = []
+    load = Load()
 
     while response != "exit":
         response = response.split(" ")
@@ -38,43 +39,7 @@ def main():
                 print(f'Emplacement "{response[1]}" incorrect')
 
         elif response[0] == "load" and len(response) >= 2:
-            if (response[1] == "mount" or response[1] == "mnt") and len(response) >= 3:
-                if response[2] == "-a":
-                    ls = cd.LsFile()
-                    for file in ls:
-                        file_load.append(cd.GetPath() + file)
-                else:
-                    file_path = cd.GetPath() + response[2]
-                    if cd.IsFile(file_path):
-                        file_load.append(file_path)
-                if len(response) >= 5 and response[3] == "\\":
-                    for file_unmount in response[4:]:
-                        file_path = cd.GetPath() + file_unmount
-                        if cd.Exist(file_path) and file_path in file_load :
-                            file_load.remove(file_path)
-            elif (response[1] == "unmount" or response[1] == "umnt") and len(response) >= 3:
-                if response[2] == "-a" and len(response) >= 3:
-                    file_load = []
-                else:
-                    file_path = cd.GetPath() + response[2]
-                    if cd.Exist(file_path):
-                        file_load.remove(file_path)
-                if len(response) >= 5 and response[3] == "\\":
-                    for file_mount in response[4:]:
-                        file_load.append(cd.GetPath() + file_mount)
-            elif response[1] == "list" or response[1] == "ls":
-                if len(file_load) == 0:
-                    print("Aucun fichier n'est chargé.")
-                else:
-                    for file in file_load:
-                        print(file)
-
-            else:
-                print(f'Commande "{response[1]}" inconnue.')
-
-
-
-
+            load.SetInput(response[1:], cd)
 
         else:
             print(f'Commande "{response[0]}" inconnue.')
