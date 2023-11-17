be2558bb0dc3e46b898a3e3b8a75e1ad04ef04ff
PGII33 <mickycomptepro@gmail.com>
2023-11-17 14:12:45 +0100

    change_occ.py :  - actualisation de ma version

--- src/lib/change_occ/change_occ.py
+++ src/lib/change_occ/change_occ.py
@@ -1,32 +1,17 @@
+class ChangeOcc:
 
-
-class FileChangeOcc:
-    def __init__(self, file_path):
+    def __init__(self, file_path=""):
+        self.as_begin = False
         self.m_file_path = file_path
 
     def SetFilePath(self, file_path):
         self.m_file_path = file_path
 
-    def GetGroup(self):
-        with open(self.m_file_path, "r") as f:
-            file = []
-            for groupe in f.read().split("\n\n"):
-                groupe = groupe.split("\n")
-                for i in range(len(groupe) - 1, -1, -1):
-                    groupe[i] = groupe[i].split(",")
-                    for j in range(len(groupe[i]) - 1, -1, -1):
-                        groupe[i][j] = groupe[i][j].replace("\n", "")
-                        if groupe[i][j] == "":
-                            groupe[i].pop(j)
-                        if groupe[i][j][0] == "#":
-                            groupe[i].pop(j)
-                    if len(groupe[i]) == 0:
-                        groupe.pop(i)
-                if len(groupe) > 0:
-                    file.append(groupe)
-        self.m_group = file
+    def Begin(self, index = 0):
+        self.as_begin = True
+        pass
 
-
-
-    def __str__(self):
-        return f"{self.m_file_path}"
+    def Change(self, directory, file):
+        if not self.as_begin:
+            self.Begin(0)
+        pass
