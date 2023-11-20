00a95143448684cb1d847110fd52c640bdf4fc23
untypequicode <untypequicode@gmail.com>
2023-11-20 17:43:12 +0100

    add GetFile & update ChangeHeader

--- src/lib/change_occ/change_header.py
+++ src/lib/change_occ/change_header.py
@@ -1,16 +1,24 @@
-from tools import ChangeOccString
+from tools import ChangeOccString, GetFile
 
 class ChangeHeader:
     def __init__(self, file_path = ""):
         self.is_begin = False
         self.m_file_path = file_path
+        self.m_header_origin = {}
+        self.m_header_new = {}
 
     def SetFilePath(self, file_path):
         self.m_file_path = file_path
 
     def Begin(self, index = 0):
+        self.m_header_origin, self.m_header_new = GetFile(self.m_file_path, index)
         self.is_begin = True
 
     def Change(self, directory, file):
         if not self.is_begin:
             self.Begin(0)
+
+a = ChangeHeader("load_file/tab_dyn_basic")
+a.Begin()
+print(a.m_header_origin)
+print(a.m_header_new)
--- src/lib/change_occ/tools.py
+++ src/lib/change_occ/tools.py
@@ -3,3 +3,18 @@ def ChangeOccString(content: str, origin: list, new: list) -> str:
     for i in range(min(len(new), len(origin))):
         new_content = new_content.replace(origin[i], new[i])
     return new_content
+
+def GetFile(file_path, index):
+    with open(file_path, "r") as file:
+        content = file.read().split("\n")
+        for i in range(len(content) - 1, -1, -1):
+            if content[i] == [] or content[i][0] == "#":
+                content.pop(i)
+            else:
+                content[i] = content[i].split(",")
+        file_origin = {content[index][0]: content[index][1:]}
+        file_new = {}
+        for i in range(len(content)):
+            if i != index:
+                file_new[content[i][0]] = content[i][1:]
+        return file_origin, file_new
