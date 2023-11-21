4be4acd099572c49d84bd9c4c8ad502729e25b4f
untypequicode <untypequicode@gmail.com>
2023-11-21 11:24:13 +0100

    update change_header.py

--- src/lib/change_occ/change_header.py
+++ src/lib/change_occ/change_header.py
@@ -14,11 +14,18 @@ class ChangeHeader:
         self.m_header_origin, self.m_header_new = GetFile(self.m_file_path, index)
         self.is_begin = True
 
-    def Change(self, directory, file):
+    def Change(self, directory, file=""):
         if not self.is_begin:
             self.Begin(0)
+        for file_new in self.m_header_new:
+            with open(directory + file_new, "r") as f:
+                text = f.read()
+                for file_origin in self.m_header_origin:
+                    print(ChangeOccString(text, self.m_header_origin[file_origin], self.m_header_new[file_new]))
+
 
 a = ChangeHeader("load_file/tab_dyn_basic")
 a.Begin()
 print(a.m_header_origin)
 print(a.m_header_new)
+a.Change("../tab_dyn/")
