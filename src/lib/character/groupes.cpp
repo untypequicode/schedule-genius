7a0d0bb5e3d0ab8c7bec976092e940187bda1b76
Micky <mickycomptepro@gmail.com>
2023-12-19 11:21:32 +0100

    Recréation de Groupes (je ne peux pas vérifier s'il fonctionne)

--- src/lib/character/groupes.cpp
+++ src/lib/character/groupes.cpp
@@ -1,104 +1,35 @@
 #include "groupes.h"
 
 Groupes::Groupes()
- : m_id(0),
-   m_nom(""),
-   m_matiere(),
-   m_horaire(),
-   m_prof(),
-   m_niveau_scolaire(0)
+    : m_id(0),
+      m_niveau_scolaire(0),
+      m_nom("/0"),
+      m_matiere(TabDynIntUnsigned()),
+      m_horaire(TabDynIntUnsigned()),
+      m_prof(TabDynIntUnsigned())
 {
 
 }
 
 Groupes::Groupes(unsigned int id,
+                 unsigned int niveau_scolaire,
                  std::string nom,
-                 TabDynString matiere,
-                 TabDynInt horaire,
-                 Profs prof,
-                 unsigned int niveau_scolaire)
- : m_id(id),
-   m_nom(nom),
-   m_matiere(matiere),
-   m_horaire(horaire),
-   m_prof(prof),
-   m_niveau_scolaire(niveau_scolaire)
-{
+                 TabDynIntUnsigned matiere,
+                 TabDynIntUnsigned horaire,
+                 TabDynIntUnsigned prof)
+    : m_id(id),
+      m_nom(nom),
+      m_matiere(matiere),
+      m_horaire(horaire),
+      m_prof(prof),
+      m_niveau_scolaire(niveau_scolaire)
+ {
 
-}
+ }
 
 Groupes::Groupes(const Groupes &groupes)
- : m_id(groupes.m_id),
-   m_nom(groupes.m_nom),
-   m_matiere(groupes.m_matiere),
-   m_horaire(groupes.m_horaire),
-   m_prof(groupes.m_prof),
-   m_niveau_scolaire(groupes.m_niveau_scolaire)
-{
-
-}
 
 Groupes::~Groupes()
 {
 
-}
-
-unsigned int Groupes::GetId()
-{
-    return m_id;
-}
-
-std::string Groupes::GetNom()
-{
-    return m_nom;
-}
-
-TabDynString Groupes::GetMatiere()
-{
-    return m_matiere;
-}
-
-TabDynInt Groupes::GetHoraire()
-{
-    return m_horaire;
-}
-
-Profs Groupes::GetProf()
-{
-    return m_prof;
-}
-
-unsigned int Groupes::GetNiveauScolaire()
-{
-    return m_niveau_scolaire;
-}
-
-void Groupes::SetId(unsigned int id)
-{
-    m_id = id;
-}
-
-void Groupes::SetNom(std::string nom)
-{
-    m_nom = nom;
-}
-
-void Groupes::SetMatiere(TabDynString matiere)
-{
-    m_matiere = matiere;
-}
-
-void Groupes::SetHoraire(TabDynInt horaire)
-{
-    m_horaire = horaire;
-}
-
-void Groupes::SetProf(Profs prof)
-{
-    m_prof = prof;
-}
-
-void Groupes::SetNiveauScolaire(unsigned int niveau_scolaire)
-{
-    m_niveau_scolaire = niveau_scolaire;
 }
--- src/lib/character/groupes.h
+++ src/lib/character/groupes.h
@@ -11,40 +11,23 @@ class Groupes
 public:
     Groupes();
     Groupes(unsigned int id,
+            unsigned int niveau_scolaire,
             std::string nom,
-            TabDynString matiere,
-            TabDynInt horaire,
-            Profs prof,
-            unsigned int niveau_scolaire);
+            TabDynIntUnsigned prof,
+            TabDynIntUnsigned matiere,
+            TabDynIntUnsigned salle);
     Groupes(const Groupes& groupes);
     ~Groupes();
 
-    unsigned int GetId();
-    unsigned int GetNiveauScolaire();
-
-    std::string GetNom();
-
-    TabDynString GetMatiere();
-    TabDynInt GetHoraire();
-
-    Profs GetProf();
-
-    void SetId(unsigned int id);
-    void SetNom(std::string nom);
-    void SetMatiere(TabDynString matiere);
-    void SetHoraire(TabDynInt horaire);
-    void SetProf(Profs prof);
-    void SetNiveauScolaire(unsigned int niveau_scolaire);
 
 private:
     unsigned int m_id;
     unsigned int m_niveau_scolaire;
     std::string m_nom;
-    TabDynString m_matiere;
-    TabDynInt m_horaire;
-    Profs m_prof;
-    MatieresTemp m_matiere_temp;
-    Salles m_salle;
+    TabDynIntUnsigned m_prof;
+    TabDynIntUnsigned m_matiere;
+    TabDynIntUnsigned m_salle;
+
 };
 
 /* SPLIT */
